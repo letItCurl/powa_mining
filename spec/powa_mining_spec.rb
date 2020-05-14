3dcc1c594231b15d59cc9235f33f8235c868a57c
letItCurl <rolandlopez.developer@gmail.com>
2020-05-14 18:14:54 +0200

    map and k=1 on context 3 test

--- spec/powa_mining_spec.rb
+++ spec/powa_mining_spec.rb
@@ -19,17 +19,32 @@ RSpec.describe PowaMining do
       @pw_two = PowaMining.new(k: @context_two[:k], data: @context_two[:data])   
       @pw_three = PowaMining.new(k: @context_three[:k], data: @context_three[:data]) 
     end
+    
     describe "#build_map" do
-      it "should build the map correctly (context_one)" do
+      
+      it "should build the map correctly (context 1)" do
         map = [[1, 7, 3, 9, 10],
                [0, 6, 2, 8,  9],
                [0, 0,-4, 2,  3],
                [0, 0, 0, 6,  7],
                [0, 0, 0, 0,  1],]
+        
         expect(@pw_one.map).to eq(map)
       end
+
+      it "should build the map correctly (context 3)" do
+        map = [[10, 1, 7, 14],
+               [ 0,-9,-3,  4],
+               [ 0, 0, 6, 13],
+               [ 0, 0, 0,  7]]
+
+        expect(@pw_three.map).to eq(map)
+      end
+    
     end
+    
     describe "case: k = 1" do
+    
       it 'should return the maximum difference of the map of context 2 (k=1)' do
         expect(@pw_two.big_money).to eq(9)
       end
@@ -37,9 +52,12 @@ RSpec.describe PowaMining do
       it 'should return the maximum difference of the map of context 3 (k=1)' do
         expect(@pw_three.big_money).to eq(13)
       end
+    
     end
+    
     xit "should return the maximum profit with context 1"do 
       expect(@pw_one.big_money).to eq(13)
     end
+  
   end
 end
