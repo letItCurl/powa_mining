0faa199659b81d8b4760e7299630a7314bd239be
letItCurl <rolandlopez.developer@gmail.com>
2020-05-14 17:01:31 +0200

    #build_map + spec

--- lib/powa_mining.rb
+++ lib/powa_mining.rb
@@ -1,27 +1,52 @@
 class PowaMining
   
-  attr_reader :k, :data
+  attr_reader :k, :data, :map
   
   def initialize(k:, data:)
     @k = k
     @data = data
+    @map = Array.new(data.length) {Array.new(data.length){0}}
+    @profit = 0
+    @count = 0
+
+    self.build_map
   end
 
-  def pretty_print
-    puts "################"
-    puts "K transaction:"
-    puts @k
-    puts "Data array:"
-    puts @data.join(',')
-    puts "################"
-    {k: @k, data: @data}
+  def pretty_print(what)
+    puts "###print nb:#{@count}###"
+    if what 
+      puts what.join(',')
+      puts "################"
+    else
+      puts "K transaction:"
+      puts @k
+      puts "Data array:"
+      puts @data.join(',')
+      puts "################"
+    end
+    @count += 1
   end
 
   def get_inputs
     {k: @k, data: @data}
   end
+  
+  def build_map
+    @map[0] = @data
+
+    i_vector = (1..@data.length-1)
+  
+    for i in i_vector do
+      for j in (i..@data.length-1) do
+        @map[i][j] = @data[j] - @data[i-1]
+      end
+    end
+  end
+
+  def big_money
+    @profit
+  end
 
 end
 # BASIC TEST
-#pm = PowaMining.new(k: 5, data: [1,2,3,4,5])
-#pm.pretty_print
+# pm = PowaMining.new(k: 5, data: [1,7,3,9,10])
--- spec/powa_mining_spec.rb
+++ spec/powa_mining_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe PowaMining do
       expect(pw.get_inputs).to eq({k: 5, data: [1,2,3]})
     end
   end
+  describe "#big_money" do
+    before do
+      @context_one = {k: 2, data: [1,7,3,9,10]}
+      @pw_one = PowaMining.new(k: @context_one[:k], data: @context_one[:data])
+    end
+    describe "#build_map" do
+      it "should build the map correctly (context_one)" do
+        map = [[1, 7, 3, 9, 10],
+               [0, 6, 2, 8,  9],
+               [0, 0,-4, 2,  3],
+               [0, 0, 0, 6,  7],
+               [0, 0, 0, 0,  1],]
+        @pw_one.build_map
+        expect(@pw_one.map).to eq(map)
+      end
+    end
+    xit "should return the maximum profit with @context_one"do 
+      expect(@pw_one.big_money).to eq(13)
+    end
+  end
 end
